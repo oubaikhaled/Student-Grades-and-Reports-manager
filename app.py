3c072861d7559563bb96a25bfaa8e50a40cd6555
oubai khaled <kbebo2203@gmail.com>
2026-09-12 23:51:59 +0300

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -596,6 +596,7 @@ class GradePortalApp:
                     else:
                         st.button("🎓 N/A", disabled=True, key=f"wa_s_na_{type_choice}_{row['id']}", use_container_width=True)
             
-            st.divider()if __name__ == "__main__":
+            st.divider()
+    if __name__ == "__main__":
     app = GradePortalApp()
     app.run()
